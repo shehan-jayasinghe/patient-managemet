57e130bab7501d722832398a6a1935fcd4f2938f
shehan-jayasinghe <jayasingheshehan1995@gmail.com>
2025-10-07 03:13:21 +0530

    add Docker support, logging configuration, and patient validation logic

--- patient-service/src/main/java/com/pm/patient_service/controller/PatientController.java
+++ patient-service/src/main/java/com/pm/patient_service/controller/PatientController.java
@@ -2,16 +2,21 @@ package com.pm.patient_service.controller;
 
 import com.pm.patient_service.dto.PatientRequestDTO;
 import com.pm.patient_service.dto.PatientResponseDTO;
-import com.pm.patient_service.mapper.PatientMapper;
+import com.pm.patient_service.dto.validators.CreatePatientValidationGroup;
 import com.pm.patient_service.service.PatientService;
-import jakarta.validation.Valid;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
+import java.util.zip.Deflater;
 
 @RestController
 @RequestMapping("/patient")
+@Tag(name = "Patient", description = "API for managing patient records")
 public class PatientController
 {
     private final PatientService patientService;
@@ -21,13 +26,27 @@ public class PatientController
     }
 
     @GetMapping
+    @Operation(summary = "Get all patients", description = "Retrieve a list of all patient records")
     public ResponseEntity<List<PatientResponseDTO>> getAllPatients() {
         List<PatientResponseDTO> patients = patientService.getAllPatients();
         return ResponseEntity.ok().body(patients);
     }
     @PostMapping
-    public ResponseEntity<PatientResponseDTO> createPatient(@Valid @RequestBody PatientRequestDTO patientRequestDTO){
+    @Operation(summary = "Create a new patient", description = "Add a new patient record to the system")
+    public ResponseEntity<PatientResponseDTO> createPatient(@Validated({Deflater.class, CreatePatientValidationGroup.class}) @RequestBody PatientRequestDTO patientRequestDTO){
         PatientResponseDTO patientResponseDTO = patientService.createPatient(patientRequestDTO);
         return ResponseEntity.ok().body(patientResponseDTO);
+    }
+    @PutMapping("/{id}")
+    @Operation(summary = "Update an existing patient", description = "Modify an existing patient record")
+    public ResponseEntity<PatientResponseDTO> updatePatient(@PathVariable UUID id, @Validated({Deflater.class}) @RequestBody PatientRequestDTO patientRequestDTO){
+        PatientResponseDTO patientResponseDTO = patientService.updatePatient(patientRequestDTO, id);
+        return ResponseEntity.ok().body(patientResponseDTO);
+    }
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a patient", description = "Remove a patient record from the system")
+    public ResponseEntity<Void> deletePatient(@PathVariable UUID id){
+        patientService.deletePatient(id);
+        return ResponseEntity.ok().build();
     }
 }
--- patient-service/src/main/java/com/pm/patient_service/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patient_service/service/PatientService.java
@@ -1,14 +1,17 @@
 package com.pm.patient_service.service;
 
-import com.pm.patient_service.controller.PatientController;
 import com.pm.patient_service.dto.PatientRequestDTO;
 import com.pm.patient_service.dto.PatientResponseDTO;
+import com.pm.patient_service.exception.EmailAlreadyExistsException;
+import com.pm.patient_service.exception.PatientNotFoundException;
 import com.pm.patient_service.mapper.PatientMapper;
 import com.pm.patient_service.model.Patient;
 import com.pm.patient_service.repository.PatientRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class PatientService {
@@ -24,7 +27,27 @@ public class PatientService {
         return patients.stream().map(PatientMapper::toDTO).toList();
     }
     public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO){
+        if(patientRepository.existsByEmail(patientRequestDTO.getEmail())){
+          throw new EmailAlreadyExistsException("A patient width this email "+ "already exists"+ patientRequestDTO.getEmail());
+        }
         Patient newPatient = patientRepository.save(PatientMapper.toModel(patientRequestDTO));
         return PatientMapper.toDTO(newPatient);
+    }
+
+    public PatientResponseDTO updatePatient(PatientRequestDTO patientRequestDTO, UUID id){
+        Patient patient = patientRepository.findById(id).orElseThrow(() -> new PatientNotFoundException("Patient not found with id: " + id));
+        if(patientRepository.existsByEmailAndIdNot(patientRequestDTO.getEmail(),id)){
+            throw new EmailAlreadyExistsException("A patient width this email "+ "already exists"+ patientRequestDTO.getEmail());
+        }
+        patient.setName(patientRequestDTO.getName());
+        patient.setEmail(patientRequestDTO.getEmail());
+        patient.setAddress(patientRequestDTO.getAddress());
+        patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
+        patient.setRegisteredDate(LocalDate.parse(patientRequestDTO.getRegisteredDate()));
+        return PatientMapper.toDTO(patientRepository.save(patient));
+    }
+
+    public void deletePatient(UUID id){
+        patientRepository.deleteById(id);
     }
 }
